e4f558e6819404a13a9d7ed11a4534f08acca049
katetap <vf.ekaterina@yandex.ru>
2020-10-11 23:18:35 +0700

    add images and text

--- app/src/main/java/com/example/wineguide/TextContentActivity.java
+++ app/src/main/java/com/example/wineguide/TextContentActivity.java
@@ -19,7 +19,14 @@ public class TextContentActivity extends AppCompatActivity {
     private int position = 0;
     private int[] array_specification = {R.string.nutritional_value, R.string.calorie_content,R.string.shelf_life};
     private int[] array_classification = {R.string.color_quality, R.string.alcohol_sugar,R.string.grape_sort};
+    private int[] array_winemaking = {R.string.technology, R.string.geography,R.string.demand};
+    private int[] array_culture = {R.string.etiquette, R.string.selection,R.string.open};
+    private int[] array_tasting = {R.string.terms, R.string.stages,R.string.places};
     private int[] array_image_specification = {R.drawable.image_nutritional_value,R.drawable.image_calorie_content,R.drawable.image_shelf_life};
+    private int[] array_image_classification = {R.drawable.image_color,R.drawable.image_alcohol,R.drawable.image_grape_sort};
+    private int[] array_image_winemaking = {R.drawable.image_technology,R.drawable.image_geography,R.drawable.image_demand};
+    private int[] array_image_culture = {R.drawable.image_etiquette,R.drawable.image_selection,R.drawable.image_open};
+    private int[] array_image_tasting = {R.drawable.image_terms,R.drawable.image_stages,R.drawable.image_places};
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,18 +54,23 @@ public class TextContentActivity extends AppCompatActivity {
                 text_content.setText(array_specification[position]);
                 break;
             case 1:
+                image_content.setImageResource(array_image_classification[position]);
                 text_content.setText(array_classification[position]);
                 break;
             case 2:
-
+                image_content.setImageResource(array_image_winemaking[position]);
+                text_content.setText(array_winemaking[position]);
                 break;
             case 3:
-
+                image_content.setImageResource(array_image_culture[position]);
+                text_content.setText(array_culture[position]);
                 break;
             case 4:
-
+                image_content.setImageResource(array_image_tasting[position]);
+                text_content.setText(array_tasting[position]);
                 break;
             case 5:
+                image_content.setImageResource(array_image_specification[position]);
 
                 break;
         }
