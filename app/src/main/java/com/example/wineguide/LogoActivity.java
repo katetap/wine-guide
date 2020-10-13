2281478bb7e45bcd21361dc087eaacf95eab297f
katetap <vf.ekaterina@yandex.ru>
2020-10-13 20:37:26 +0700

    create activity with logo

---
+++ app/src/main/java/com/example/wineguide/LogoActivity.java
@@ -0,0 +1,28 @@
+package com.example.wineguide;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class LogoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logo_activity);
+    }
+
+    private void onClickStart (View view) {
+        Intent i = new Intent(LogoActivity.this, MainActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+}
